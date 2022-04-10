36498aea6b01be6bedc0c30f94f789a3fa4dc8f3
justinakelly <justinakelly@udesa.edu.ar>
2022-04-10 15:50:48 -0300

    agregue imagenes e info a db

--- prog-2-project/db/artworksData.js
+++ prog-2-project/db/artworksData.js
@@ -4,9 +4,75 @@
 
 const artworksInfo = [
     {
-        image : '',
-        name : 'justina',
-        description : 'hola',
+        image : '../public/images/products/garden-of-forking-paths.jpeg',
+        name : 'GARDEN OF FORKING PATHS',
+        description : '"This web of time - the strands of which approach one another, bifurcate, intersect or ignore each other through the centuries - embraces every possibility. We do not exist in most of them. In some, you exist and not I, while in others I do, and you do not" -— Jorge Luis Borges',
+        date : '22-5',
+    },
+    {
+        image : '../public/images/products/a-place-with-no-memory.jpeg',
+        name : 'A PLACE WITH NO MEMORY',
+        description : '"...That is where I want to live the rest of my life. A warm place with no memory.” ―- Stephen King',
+        date : '22-5',
+    },
+    {
+        image : '../public/images/products/.jpeg',
+        name : '',
+        description : '',
+        date : '22-5',
+    },
+    {
+        image : '../public/images/products/.jpeg',
+        name : '',
+        description : '',
+        date : '22-5',
+    },
+    {
+        image : '../public/images/products/.jpeg',
+        name : '',
+        description : '',
+        date : '22-5',
+    },
+    {
+        image : '../public/images/products/.jpeg',
+        name : '',
+        description : '',
+        date : '22-5',
+    },
+    {
+        image : '../public/images/products/.jpeg',
+        name : '',
+        description : '',
+        date : '22-5',
+    },
+    {
+        image : '../public/images/products/.jpeg',
+        name : '',
+        description : '',
+        date : '22-5',
+    },
+    {
+        image : '../public/images/products/.jpeg',
+        name : '',
+        description : '',
+        date : '22-5',
+    },
+    {
+        image : '../public/images/products/.jpeg',
+        name : '',
+        description : '',
+        date : '22-5',
+    },
+    {
+        image : '../public/images/products/.jpeg',
+        name : '',
+        description : '',
+        date : '22-5',
+    },
+    {
+        image : '../public/images/products/.jpeg',
+        name : '',
+        description : '',
         date : '22-5',
     }
 ]
