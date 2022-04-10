191f22839bb33a208e9034ae85c111d4de2fd712
justinakelly <justinakelly@udesa.edu.ar>
2022-04-10 16:34:44 -0300

    intento de hacer ruta de un producto

--- prog-2-project/routes/artworks.js
+++ prog-2-project/routes/artworks.js
@@ -3,16 +3,8 @@ var router = express.Router();
 
 var moduleArtworks = require('../db/artworksData')
 
-
-router.get('/', function(req, res){
-   res.send(moduleArtworks.getAll()) //luego cambiarlo a res.render que muestra html
-    console.log('eaaaa')
-});
-
-
-
 router.get('/artworks', function(req, res){
-    
+    res.send(moduleArtworks)
 });
 
 module.exports = router;
--- prog-2-project/routes/index.js
+++ prog-2-project/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var moduleArtworks = require('../db/artworksData')
+
+router.get('/', function(req, res){
+  res.send(moduleArtworks.getAll()) //luego cambiarlo a res.render que muestra html
 });
+
+/* GET home page. */ //welcome to express
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
 module.exports = router;
