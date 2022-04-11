8973295e78b43f3441ebbf30987775a4d473b574
justinakelly <justinakelly@udesa.edu.ar>
2022-04-11 10:22:17 -0300

    mejore controllers

--- prog-2-project/controllers/artworksController.js
+++ prog-2-project/controllers/artworksController.js
@@ -5,12 +5,13 @@ const controller = {
 
     }, //no lo entiendo, no serie en index?
     show: function(req, res){
-        const result = moduleArtworks
+        const result = moduleArtworks[0]
+        //const result = moduleArtworks[req.params.id - 1]
         res.send(result)
 
     },
     add: function(req, res){
-
+        res.send('hola')
     }
    
 };
