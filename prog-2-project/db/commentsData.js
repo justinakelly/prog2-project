e4a0bc51eeea8d8922efd23cc9335b45380fdcc6
justinabsegui <mbasombriosegui@udesa.edu.ar>
2022-04-13 23:56:23 -0300

    index comments

--- prog-2-project/db/commentsData.js
+++ prog-2-project/db/commentsData.js
@@ -5,9 +5,9 @@
 // imagen de perfil: no es necesario que coloques una imagen de perfil. Podés dejar la que está ahora en la maqueta.
 
 let commentsInfo = [
-    {  username: 'justinabsegui', comment : 'Wow es super modernoso',  profilePicture : '/images/users/1.png' },
-    {  username: 'justinakelly', comment : 'Nunca vi un nft asi',  profilePicture : '/images/users/2.png' },
-    {  username: 'candelariatallavi', comment : 'Quiero conocer al artista',  profilePicture : '/images/users/3.png' },
-    {  username: 'pehuen', comment : 'Cuantos ETH vale esto?',  profilePicture : '/images/users/4.png' },
-    {  username: 'luis', comment : 'Conozco al artista, prodigio',  profilePicture : '/images/users/5.png' },
+    {  username: 'justinabsegui', comment : 'Wow thats so cool :)',  profilePicture : '/images/users/1.png' },
+    {  username: 'justinakelly', comment : 'Never seen sthing like this',  profilePicture : '/images/users/2.png' },
+    {  username: 'candelariatallavi', comment : 'I wanna meet the artist behind this',  profilePicture : '/images/users/3.png' },
+    {  username: 'pehuen', comment : 'How much ETH is it worth?',  profilePicture : '/images/users/4.png' },
+    {  username: 'luis', comment : 'I offer a secure P2P transaction for this NFT',  profilePicture : '/images/users/5.png' },
 ]
