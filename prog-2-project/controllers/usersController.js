ed19a3184c70dd854fedb9d17659d6ad6a534a91
justinabsegui <mbasombriosegui@udesa.edu.ar>
2022-04-13 22:32:58 -0300

    usercontroller,user

--- prog-2-project/controllers/usersController.js
+++ prog-2-project/controllers/usersController.js
@@ -3,23 +3,23 @@ var artworks = require('../db/artworksData');
 
 const controller = {
     register: function (req, res) {
-        res.render('register');
-    },
+    res.render('register');
+},
     login: function (req, res) {
-        res.render('login');
-    },
+    res.render('login');
+},
     profile: function (req, res) {
-        res.render('profile', {
-            user: user,
-            artworks: artworksData,
-        });
-    },
+    res.render('profile', {
+        user: user,
+        artworks: artworksData,
+    });
+},
     edit: function (req, res) {
-        res.render('edit', {
-            user: user,
-        });
-    }
-    }
+    res.render('edit', {
+        user: user,
+    });
+}
+}
 
 
 module.exports = controller;
