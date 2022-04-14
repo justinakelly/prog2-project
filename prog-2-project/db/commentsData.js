80ae9d7fe55ca5ee356b3b9df8db97cd15b7df15
justinabsegui <mbasombriosegui@udesa.edu.ar>
2022-04-13 22:41:55 -0300

    comments

--- prog-2-project/db/commentsData.js
+++ prog-2-project/db/commentsData.js
@@ -5,9 +5,9 @@
 // imagen de perfil: no es necesario que coloques una imagen de perfil. Podés dejar la que está ahora en la maqueta.
 
 let commentsInfo = [
-    {  username: '', commentary : '',  profilePicture : '' },
-    {  username: '', commentary : '',  profilePicture : '' },
-    {  username: '', commentary : '',  profilePicture : '' },
-    {  username: '', commentary : '',  profilePicture : '' },
-    
+    {  username: 'justinabsegui', comment : 'Wow es super modernoso',  profilePicture : '' },
+    {  username: 'justinakelly', comment : 'Nunca vi un nft asi',  profilePicture : '' },
+    {  username: 'candelariatallavi', comment : 'Quiero conocer al artista',  profilePicture : '' },
+    {  username: 'pehuen', comment : 'Cuantos ETH vale esto?',  profilePicture : '' },
+    {  username: 'luis', comment : 'Conozco al artista, prodigio',  profilePicture : '' },
 ]
