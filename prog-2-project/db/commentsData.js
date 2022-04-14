f059b56e77d84a0392f6440c7042593a61562ce2
justinabsegui <mbasombriosegui@udesa.edu.ar>
2022-04-13 22:45:36 -0300

    profile pics

--- prog-2-project/db/commentsData.js
+++ prog-2-project/db/commentsData.js
@@ -5,9 +5,9 @@
 // imagen de perfil: no es necesario que coloques una imagen de perfil. Podés dejar la que está ahora en la maqueta.
 
 let commentsInfo = [
-    {  username: 'justinabsegui', comment : 'Wow es super modernoso',  profilePicture : '' },
-    {  username: 'justinakelly', comment : 'Nunca vi un nft asi',  profilePicture : '' },
-    {  username: 'candelariatallavi', comment : 'Quiero conocer al artista',  profilePicture : '' },
-    {  username: 'pehuen', comment : 'Cuantos ETH vale esto?',  profilePicture : '' },
-    {  username: 'luis', comment : 'Conozco al artista, prodigio',  profilePicture : '' },
+    {  username: 'justinabsegui', comment : 'Wow es super modernoso',  profilePicture : '/images/users/1.png' },
+    {  username: 'justinakelly', comment : 'Nunca vi un nft asi',  profilePicture : '/images/users/2.png' },
+    {  username: 'candelariatallavi', comment : 'Quiero conocer al artista',  profilePicture : '/images/users/3.png' },
+    {  username: 'pehuen', comment : 'Cuantos ETH vale esto?',  profilePicture : '/images/users/4.png' },
+    {  username: 'luis', comment : 'Conozco al artista, prodigio',  profilePicture : '/images/users/5.png' },
 ]
