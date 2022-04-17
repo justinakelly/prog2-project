b2cd60adad20ef4bac968b1c4bfc546fb97d8232
justinakelly <justinakelly@udesa.edu.ar>
2022-04-17 17:21:25 -0300

    prolijidad

--- prog-2-project/routes/index.js
+++ prog-2-project/routes/index.js
@@ -3,20 +3,7 @@ var router = express.Router();
 var controller = require('../controllers/indexController');
 
 
-router.get('/', controller.index);//luego cambiarlo a res.render que muestra html
- //index.html
-
-// router.get('/login', controller.login); //login.html
-
-//  router.get('/register', controller.register); //register.html lo acomode para q te mande desde users/login y register
-
- router.get('/results', controller.results);
-
-
-
-/* GET home page. */ //welcome to express
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
+router.get('/', controller.index);  //index.ejs
+router.get('/results', controller.results); //search-results.ejs
 
 module.exports = router;
