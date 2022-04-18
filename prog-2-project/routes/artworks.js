cce66839a5110e50afd07397f5501aa06a3ffebb
candetallavi <ctallavi@udesa.edu.ar>
2022-04-18 09:42:30 -0300

    ordenando

--- prog-2-project/routes/artworks.js
+++ prog-2-project/routes/artworks.js
@@ -2,8 +2,6 @@ var express = require('express');
 var router = express.Router();
 var controller = require('../controllers/artworksController')
 
-
-//router.get('/', controller.index); muestra todos los productos
 router.get('/add', controller.add); //product-add.ejs
 router.get('/:id', controller.show); //product.ejs --> muestra detalle
 
