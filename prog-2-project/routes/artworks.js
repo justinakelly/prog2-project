813456f363663535937c20d15e157ae8d8ad43f5
justinabsegui <mbasombriosegui@udesa.edu.ar>
2022-06-04 22:24:27 -0300

    multer

--- prog-2-project/routes/artworks.js
+++ prog-2-project/routes/artworks.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var controller = require('../controllers/artworksController')
+var multer = require ('multer');
+const upload= multer ({dest:'public/images/uploads'}); //inicializar multer
 
 router.get('/add', controller.add); //product-add.ejs
+router.post('/add', upload.single('Image1'), controller.store);
 router.get('/:id', controller.show); //product.ejs --> muestra detalle
 
 
