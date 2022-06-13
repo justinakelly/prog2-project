e8ef19d9feedf07af7665405452d193b06ef626b
candetallavi <ctallavi@udesa.edu.ar>
2022-06-13 20:35:00 -0300

    detalledeP

--- prog-2-project/controllers/usersController.js
+++ prog-2-project/controllers/usersController.js
@@ -3,9 +3,9 @@ var db = require('../database/models');
 const controller = {
 
     profile: function (req, res) {
-            db.User.findByPk(req.session.user.id, { include: [ { association: 'artworks' } ] })
-            .then(function (user) {
-                res.render('profile', { user });
+            db.User.findByPk(req.session.users.id, { include: [ { association: 'artworks' } ] })
+            .then(function (users) {
+                res.render('profile', { users });
             })
             .catch(function (error) {
                 res.send(error)
@@ -13,33 +13,33 @@ const controller = {
     },
 
     edit: function (req, res) {             
-        res.render('profile-edit', { user: data.user });
+        res.render('profile-edit', { users: data.users });
     },
 
     access: function(req, res, next) {
         db.User.findOne({ where: { username: req.body.username }}) // busco usuario en db, en where busco lo que se mando por formulario de login
-            .then(function(user) { //resultado de promesa=usuario
-                if (!user) throw Error('User not found.') 
-                if (hasher.compareSync(req.body.password, user.password)) {// ver si la contrasena esta bien, compara lo que ingresa usr con hash de db
-                    req.session.user = user; //guardo en campo usuario (servidor) datos del usuario, si es true entra a if
+            .then(function(users) { //resultado de promesa=usuario
+                if (!users) throw Error('User not found.') 
+                if (hasher.compareSync(req.body.password, users.password)) {// ver si la contrasena esta bien, compara lo que ingresa usr con hash de db
+                    req.session.users = users; //guardo en campo usuario (servidor) datos del usuario, si es true entra a if
                     if (req.body.rememberme) { //si apreta boton
-                        res.cookie('userId', user.id, { maxAge: 1000  ,60  :60 * 7 })// cookie nueva que se guarda en cliente por 7 hs
+                        res.cookie('usersId', users.id, { maxAge: 1000  ,60  :60 * 7 })// cookie nueva que se guarda en cliente por 7 hs
                     }
                     res.redirect('/');
                 } else {
                     throw Error('Invalid credentials.')
                 }
             })
-            .catch(function (err) {
-                next(err)
+            .catch(function (error) {
+                next(error)
             })
     },
     login: function(req, res) {
         res.render('login', { title: 'Login'});
     },
     logout: function (req, res, next) {
-        req.session.user = null;
-        res.clearCookie('userId');
+        req.session.users = null;
+        res.clearCookie('usersId');
         res.redirect('/')
     },
     register: function(req, res) {
