5c515f83eb786e7f9334cd45cce64e3970302907
justinabsegui <mbasombriosegui@udesa.edu.ar>
2022-06-13 16:08:12 -0300

    artwcontroller

--- prog-2-project/controllers/indexController.js
+++ prog-2-project/controllers/indexController.js
@@ -3,20 +3,71 @@ const artwork = db.Artwork;
 
 const controller = {
 //nos llevamos/cortamos el callback, el segundo argumento de la funcion de las rutas
-   
 
-    index:  function (req, res){
-           db.Artwork.findAll()
-            .then(function(artworks){
-                res.send(artworks);
-            } )
-        // function(req, res){
-        // artwork.findAll()
-        // .then(
-        //     data=> res.render(data)
-        // )  
-        // .catch(error=>console.log(error))
+index: function(req, res) {
+    res.redirect('/');
+},
+results: function(req, res) {
+    db.Artwork.findAll({ 
+        where: {
+            [op.or]: [
+                { title: { [op.like]: "%"+req.query.criteria+"%"} },
+                { author: { [op.like]: "%"+req.query.criteria+"%"} }
+            ]
+        },
+        include: [ { association: 'owner' } ] 
+    }).then(function (books) {
+            res.render('search-results', { books });
+        })
+        .catch(function (error) {
+            res.send(error)
+        });
+},
+access: function(req, res, next) {
+    db.User.findOne({ where: { username: req.body.username }}) // busco usuario en db, en where busco lo que se mando por formulario de login
+        .then(function(user) { //resultado de promesa=usuario
+            if (!user) throw Error('User not found.') 
+            if (hasher.compareSync(req.body.password, user.password)) {// ver si la contrasena esta bien, compara lo que ingresa usr con hash de db
+                req.session.user = user; //guardo en campo usuario (servidor) datos del usuario, si es true entra a if
+                if (req.body.rememberme) { //si apreta boton
+                    res.cookie('userId', user.id, { maxAge: 1000 * 60 * 60 * 7 })// cookie nueva que se guarda en cliente por 7 hs
+                }
+                res.redirect('/');
+            } else {
+                throw Error('Invalid credentials.')
+            }
+        })
+        .catch(function (err) {
+            next(err)
+        })
+},
+login: function(req, res) {
+    res.render('login', { title: 'Login'});
+},
+logout: function (req, res, next) {
+    req.session.user = null;
+    res.clearCookie('userId');
+    res.redirect('/')
+},
+register: function(req, res) {
+    res.render('register');
+},
 
+store: function(req, res) {
+    if (!req.body.email) { throw Error('No email provided.') }
+    const hashedPassword = hasher.hashSync(req.body.password, 10);
+    db.User.create({
+            username: req.body.username,
+            password: hashedPassword,
+            email: req.body.email,
+        })
+        .then(function () {
+            res.redirect('/');
+        })
+        .catch(function (error) {
+            res.send(error);
+        })
+},
         // randomArtwork = data.artworks
         // function randomNum () {
         //     return Math.floor(Math.random() * randomArtwork.length);
@@ -42,7 +93,7 @@ const controller = {
             
         //                     });
 
-    },
+    
     
     // results: function (req,res){
     //     res.render ('search-results', { 
@@ -51,30 +102,14 @@ const controller = {
     //                                 });
     
     // },
-    results: function(req, res) {
-        db.Artwork.findAll({ 
-            where: {
-                [op.or]: [
-                    { title: { [op.like]: "%"+req.query.criteria+"%"} },
-                    { author: { [op.like]: "%"+req.query.criteria+"%"} }
-                ]
-            },
-            include: [ { association: 'owner' } ] 
-        }).then(function (artworks) {
-                res.render('search-results', { artworks });
-            })
-            .catch(function (error) {
-                res.send(error)
-            });
-    },
-
+   
     // prueba: function (req, res){
     //    db.Artwork.findAll()
     //     .then(function(artworks){
-    //         res.render(artworks);
+    //         res.send(artworks);
     //     } )
         
-    // }
+    // },
 }; 
 
 module.exports = controller;
