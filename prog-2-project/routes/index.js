8fd9e661499414ca77747a1c2b88cc97d9cfe99e
candetallavi <ctallavi@udesa.edu.ar>
2022-06-13 10:38:57 -0300

    base de datos

--- prog-2-project/routes/index.js
+++ prog-2-project/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var controller = require('../controllers/indexController');
+var db = require('../database/models');
 
 
 router.get('/', controller.index);  //index.ejs
