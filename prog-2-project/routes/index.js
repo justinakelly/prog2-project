e3a9986d6d2ede91c8a6aafb6e4bab185ba4c74e
justinakelly <justinakelly@udesa.edu.ar>
2022-06-16 13:36:32 -0300

    rutas

--- prog-2-project/routes/index.js
+++ prog-2-project/routes/index.js
@@ -7,12 +7,4 @@ router.get('/', controller.index);  //index.ejs
 router.get('/results', controller.results); //search-results.ejs
 
 
-router.get('/login', controller.login);
-router.post('/login', controller.access);
-
-router.get('/logout', controller.logout);
-
-router.get('/register', controller.register);
-router.post('/register', controller.store);
-
 module.exports = router;
--- prog-2-project/routes/users.js
+++ prog-2-project/routes/users.js
@@ -4,6 +4,15 @@ var controller = require('../controllers/usersController');
 
 router.get('/me', controller.profile); 
 router.get('/me/edit', controller.edit); 
+
+router.get('/login', controller.login);
+router.post('/login', controller.access);
+
+router.get('/logout', controller.logout);
+
+router.get('/register', controller.register);
+router.post('/register', controller.store);
+
 // router.post('/me/edit', controller.edit);
 
 // router.get('/:id', controller.profile); //profile.ejs
