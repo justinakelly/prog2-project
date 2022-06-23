2656d36492f724908d1754507adacf4c9f8fdf9c
candetallavi <ctallavi@udesa.edu.ar>
2022-06-23 08:00:10 -0300

    acomode

--- prog-2-project/database/models/artwork.js
+++ prog-2-project/database/models/artwork.js
@@ -25,7 +25,7 @@ module.exports = function (sequelize, dataTypes) {
     const Artwork = sequelize.define (alias , cols, configs);
 
     Artwork.associate = function(models){//callback que recibe como param models
-        Artwork.belongsTo( models.User, { // ol donde explico relacion
+        Artwork.belongsTo( models.User, { // 
             as: 'creator', // usuario que carga artwork, campo donde vive modelo asociado
             foreignKey: 'user_id'// columna que conecta al artwork con id de usuario
         })
